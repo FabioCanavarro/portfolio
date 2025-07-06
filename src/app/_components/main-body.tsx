a6a584fc517ec98596ee7a7da226678ca37f9afe
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-07-06 23:20:13 +0700

    improved animation for mobile

--- src/app/_components/main-body.tsx
+++ src/app/_components/main-body.tsx
@@ -123,7 +123,7 @@ const MainBody = () => {
           <motion.section 
             className="mb-24 relative"
             initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.5 }}
             transition={{ duration: 0.7 }}
           >
