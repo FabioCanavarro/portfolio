9a9ddc82ad8bddb0649d7aac2df2ed5653d932dc
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-07-07 20:26:46 +0700

    prototype?

--- src/app/_components/main-body.tsx
+++ src/app/_components/main-body.tsx
@@ -15,6 +15,7 @@ import {
 } from "lucide-react";
 import AnimatedText from "./animated-text";
 import Link from "next/link";
+import AboutMe from "./about-me";
 
 const projects = [
   {
@@ -154,46 +155,7 @@ const MainBody = () => {
           </motion.div>
         </section>
 
-        {/* About Section */}
-        <motion.section
-          className="mb-24 relative"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.7 }}
-        >
-          <div id="about" className="absolute -top-24"></div>
-          <h2 className="text-3xl font-bold mb-6 text-rosewater flex items-center">
-            <Sparkles className="w-6 h-6 mr-3" />
-            About Me
-          </h2>
-          <div className="space-y-4 text-subtext0 leading-relaxed bg-crust/50 p-6 rounded-xl border border-surface0 backdrop-blur-sm shadow-lg shadow-crust/50">
-            <p>
-              As a 16-year-old software developer, I specialize in systems
-              programming with a primary focus on Rust, complemented by a strong
-              foundation in Python. While my passion lies with low-level
-              development, I am also familiar with languages like C++, Java,
-              TypeScript, HTML, and CSS from various projects. I&apos;m
-              captivated by the intricate &quot;ins and outs&quot; of
-              computing—the challenge and control that come with low-level
-              development. The incredible leap in performance from early
-              computers to today&apos;s systems inspires me to engineer software
-              that is both powerful and efficient.
-            </p>
-            <p>
-              Currently, I am diving deeper into asynchronous programming in
-              Rust, focusing on advanced benchmarking and optimization
-              techniques to push my code to its limits. My ambition extends to
-              building foundational technologies from the ground up, with
-              long-term goals that include developing a complete operating
-              system, creating a feature-rich database engine named TransientDB,
-              and evolving my assembly project into a full-fledged programming
-              language. I am driven by the pursuit of solving complex problems
-              and engineering the robust, high-performance systems of the
-              future.
-            </p>
-          </div>
-        </motion.section>
+      <AboutMe /> 
 
         {/* Experience Section */}
         <motion.section
